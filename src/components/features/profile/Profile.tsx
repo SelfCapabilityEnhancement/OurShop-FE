4463715aacb7c5862cb175655ccdbe8f36125ada
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-09-27 14:33:08 +0800

    style(#1): add divide between profile options

--- src/components/features/profile/Profile.tsx
+++ src/components/features/profile/Profile.tsx
@@ -27,27 +27,31 @@ export default function Profile() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5">
-          <Menu.Item>
-            {({ active }) => (
-              <NavLink
-                to="my-wallet"
-                className={classNames(active ? 'bg-gray-100' : '', 'wallet block px-4 py-2 text-center text-base')}
-              >
-                my wallet
-              </NavLink>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <NavLink
-                to="my-order"
-                className={classNames(active ? 'bg-gray-100' : '', 'settings block px-4 py-2 text-center text-base')}
-              >
-                settings
-              </NavLink>
-            )}
-          </Menu.Item>
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-40 rounded-md bg-white py-1 shadow-lg divide-y divide-gray-200 ring-1 ring-black ring-opacity-5">
+          <div className="px-1 py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <NavLink
+                  to='my-wallet'
+                  className={classNames(active ? 'bg-gray-100' : '', 'wallet block px-4 py-2 text-center text-base')}
+                >
+                  my wallet
+                </NavLink>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="px-1 py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <NavLink
+                  to='my-order'
+                  className={classNames(active ? 'bg-gray-100' : '', 'settings block px-4 py-2 text-center text-base')}
+                >
+                  settings
+                </NavLink>
+              )}
+            </Menu.Item>
+          </div>
         </Menu.Items>
       </Transition>
     </Menu>
