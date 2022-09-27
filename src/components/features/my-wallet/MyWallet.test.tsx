c7e65c43901e9951a925d9a5acfb7b573270c926
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-09-27 14:08:21 +0800

    feat(#1): implement wallet page

--- src/components/features/my-wallet/MyWallet.test.tsx
+++ src/components/features/my-wallet/MyWallet.test.tsx
@@ -1,10 +1,12 @@
 import { Container } from 'react-dom';
-import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import MyWallet from '@/components/features/my-wallet/MyWallet';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('display wallet info', () => {
   let container: Container;
+  const user = userEvent.setup();
 
   beforeEach(() => {
     container = render(<MyWallet />, { wrapper: BrowserRouter }).container;
@@ -17,5 +19,19 @@ describe('display wallet info', () => {
 
     // then
     expect(wallet).toBeTruthy();
+    expect(screen.getByText('My Tokens')).toBeTruthy();
+    expect(screen.getByText('My Bank Account')).toBeTruthy();
+    expect(screen.getByText('Good to Go')).toBeTruthy();
+  });
+
+  it('should navigate to home when click good to go', async () => {
+    // given
+    const button = container.querySelector('.go-home');
+
+    // when
+    await user.click(button as Element);
+
+    // then
+    expect(screen.findByText('HomePage')).toBeTruthy();
   });
 });
