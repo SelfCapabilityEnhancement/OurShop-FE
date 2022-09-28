ab075bbe66741d10dc0b442535dfd47c2c6c92b0
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-09-28 15:00:02 +0800

    feat(#3): extract products as props

--- src/components/features/shopping-cart/ShoppingCart.test.tsx
+++ src/components/features/shopping-cart/ShoppingCart.test.tsx
@@ -1,22 +1,36 @@
 import {Container} from 'react-dom';
 import {render} from '@testing-library/react';
-import ShoppingCart from '@/components/features/shopping-cart/ShoppingCart';
+import ShoppingCart, {Product} from '@/components/features/shopping-cart/ShoppingCart';
 import {BrowserRouter} from 'react-router-dom';
 
-describe('display profile option', () => {
+describe('display shopping cart page given nonempty products', () => {
   let container: Container;
+  const tempProducts:Array<Product> = [
+    {name: 'Product1', token: 5, count: 1},
+    {name: 'Product2', token: 3, count: 2},
+    {name: 'Product3', token: 2, count: 3},
+  ];
 
   beforeEach(() => {
-    container = render(<ShoppingCart/>, {wrapper: BrowserRouter}).container;
+    container = render(<ShoppingCart products={tempProducts}/>, {wrapper: BrowserRouter}).container;
   });
-  it('should display shopping cart list', async () => {
+  it('should display shopping cart list', () => {
     const products = container.querySelectorAll('.product');
 
     expect(products).toHaveLength(3);
   });
-  it('should display pay button', async () => {
+  it('should display pay button', () => {
     const payBtn = container.querySelectorAll('.payBtn');
 
     expect(payBtn.length).toBe(2);
+  });
+});
+
+describe('display empty message given empty products', () => {
+  it('should display nothing in the shopping cart', () => {
+    const container = render(<ShoppingCart products={[]}/>, {wrapper: BrowserRouter}).container;
+    const emptyMsg = container.querySelector('.empty-msg');
+
+    expect(emptyMsg).toBeTruthy();
   });
 });
--- src/components/features/shopping-cart/ShoppingCart.tsx
+++ src/components/features/shopping-cart/ShoppingCart.tsx
@@ -1,15 +1,22 @@
+export interface Product {
+  name: string
+  token: number
+  count: number
+}
+const tempProducts:Array<Product> = [
+  {name: 'Product1', token: 5, count: 2},
+  {name: 'Product2', token: 3, count: 1},
+  {name: 'Product3', token: 2, count: 3},
+];
 
-export default function ShoppingCart() {
-  const products = [
-    {name: 'Product1', token: 5, count: 1},
-    {name: 'Product2', token: 3, count: 2},
-    {name: 'Product3', token: 2, count: 3},
-  ];
-
+export default function ShoppingCart({ products=tempProducts }: { products: Array<Product> }) {
   return (
     <div className="w-5/6 h-[calc(100vh-150px)] mx-auto mt-5 relative">
-      <ul className="flex flex-col">
-        {products.map(({name, count}, index) => {
+      {products.length === 0 ? <div className="empty-msg text-center text-3xl font-semibold mt-10">
+        nothing in the shopping cart
+      </div> : (<div>
+        <ul className="flex flex-col">
+          {products.map(({ name, count }, index) => {
           return (
             <li key={`product-${index}`} className="product border-gray-400 my-5 h-20">
               <div className="flex flex-row transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white dark:bg-gray-800 rounded-md items-center p-4">
@@ -33,15 +40,16 @@ export default function ShoppingCart() {
             </li>
           );
         })}
-      </ul>
-      <button type="button"
-              className="payBtn go-home w-1/6 px-3 py-3 h-12 absolute bottom-10 right-96 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 ">
+        </ul>
+        <button type="button"
+        className="payBtn go-home w-1/6 px-3 py-3 h-12 absolute bottom-10 right-96 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 ">
         Pay by Token
-      </button>
-      <button type="button"
-              className="payBtn go-home w-1/6 px-3 py-3 h-12 absolute bottom-10 right-2 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 ">
+        </button>
+        <button type="button"
+        className="payBtn go-home w-1/6 px-3 py-3 h-12 absolute bottom-10 right-2 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 ">
         Pay by Money
-      </button>
+        </button>
+      </div>) }
     </div>
   );
 }
