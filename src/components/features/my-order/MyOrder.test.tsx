cc10b001bf6aa0bcd9e1ddd8866ea4c80acdf2d4
linxian.wu <linxian.wu@thoughtworks.com>
2022-09-28 15:10:47 +0800

    feat(#4): display all orderItems in MyOrder page and test

--- src/components/features/my-order/MyOrder.test.tsx
+++ src/components/features/my-order/MyOrder.test.tsx
@@ -1,5 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import MyOrder from '@/components/features/my-order/MyOrder';
+import { Container } from 'react-dom';
+import userEvent from '@testing-library/user-event/index';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('display my order', () => {
   const mockOrder = [
@@ -8,9 +11,18 @@ describe('display my order', () => {
       purchaseDate: '11.10.2022',
       purchaseNumber: '1',
     },
+    {
+      productName: 'Product Name2',
+      purchaseDate: '11.12.2022',
+      purchaseNumber: '2',
+    },
   ];
+  let container: Container;
 
   beforeEach(() => {
-    render(<MyOrder />);
+    container = render(<MyOrder />, { wrapper: BrowserRouter }).container;
+  });
+  test('render all OrderItems', () => {
+    expect(container.querySelectorAll('.order-item').length).toBe(2);
   });
 });
--- src/components/features/my-order/MyOrder.tsx
+++ src/components/features/my-order/MyOrder.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import OrderItem from '@/components/features/my-order/OrderItem';
 
-const Orders = [
+const orders = [
   {
+    id: 1,
     productName: 'Product Name',
     purchaseDate: '11.10.2022',
     purchaseNumber: '1',
+  },
+  {
+    id: 2,
+    productName: 'Product Name2',
+    purchaseDate: '11.12.2022',
+    purchaseNumber: '2',
   },
 ];
 export default function MyOrder() {
   return (
     <div className="w-[1280px] mx-auto mt-5">
       <ul className="flex flex-col">
-        <li className="product border-gray-400 mb-2 h-20 ">
-          <OrderItem order={Orders[0]} />
-        </li>
+        {orders.map((item) => (
+          <li key={item.id} className="order-item product border-gray-400 mb-5 h-20 ">
+            <OrderItem order={item} />
+          </li>
+        ))}
       </ul>
     </div>
   );
