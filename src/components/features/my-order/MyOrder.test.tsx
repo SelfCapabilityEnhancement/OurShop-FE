cdaba58f594c5726b246764c9d4e0859aab1db99
linxian.wu <linxian.wu@thoughtworks.com>
2022-09-28 11:43:10 +0800

    feat(#4): test for render all elements in MyOrder in features

---
+++ src/components/features/my-order/MyOrder.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import MyOrder from '@/components/features/my-order/MyOrder';
+
+describe('display my order', () => {
+  const mockOrder = [
+    {
+      productName: 'Product Name',
+      purchaseDate: '11.10.2022',
+      purchaseNumber: 1,
+    },
+  ];
+
+  beforeEach(() => {
+    render(<MyOrder />);
+  });
+  test('should render picture, product name, purchase date and number in my order', async () => {
+    expect(
+      screen.getByTestId('product-picture').textContent
+    ).toBeInTheDocument();
+    expect(screen.getByTestId('product-name').textContent).toBe(
+      mockOrder[0].productName
+    );
+    expect(screen.getByTestId('purchase-date-title').textContent).toBe(
+      'Date of purchase: '
+    );
+    expect(screen.getByTestId('product-date').textContent).toBe(
+      mockOrder[0].purchaseDate
+    );
+    expect(screen.getByTestId('number-title').textContent).toBe('number: ');
+  });
+  expect(screen.getByTestId('purchase-numver').textContent).toBe(
+    mockOrder[0].purchaseNumber
+  );
+});
