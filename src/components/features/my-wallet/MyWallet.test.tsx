4a0aa4dc39541d6d086ac340e49fc09326e504da
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-09-28 09:47:26 +0800

    chore(#1): remove unnecessary comments

--- src/components/features/my-wallet/MyWallet.test.tsx
+++ src/components/features/my-wallet/MyWallet.test.tsx
@@ -12,12 +12,8 @@ describe('display wallet info', () => {
     container = render(<MyWallet />, { wrapper: BrowserRouter }).container;
   });
   it('should display wallet info', async () => {
-    // given
-
-    // when
     const wallet = container.querySelector('.wallet-header');
 
-    // then
     expect(wallet).toBeTruthy();
     expect(screen.getByText('My Tokens')).toBeTruthy();
     expect(screen.getByText('My Bank Account')).toBeTruthy();
@@ -25,13 +21,10 @@ describe('display wallet info', () => {
   });
 
   it('should navigate to home when click good to go', async () => {
-    // given
     const button = container.querySelector('.go-home');
 
-    // when
     await user.click(button as Element);
 
-    // then
     expect(screen.findByText('HomePage')).toBeTruthy();
   });
 });
--- src/components/features/profile/Profile.test.tsx
+++ src/components/features/profile/Profile.test.tsx
@@ -13,27 +13,21 @@ describe('display profile option', () => {
     container = render(<Profile />, { wrapper: BrowserRouter }).container;
   });
   it('should display options when click avatar', async () => {
-    // given
     const avatar = container.querySelector('.avatar');
 
-    // when
     await user.click(avatar as Element);
 
-    // then
     expect(container.querySelector('.wallet')).toBeTruthy();
     expect(container.querySelector('.settings')).toBeTruthy();
   });
 
   it('should navigate to wallet page when click my wallet', async () => {
-    // given
     const avatar = container.querySelector('.avatar');
 
-    // when
     await user.click(avatar as Element);
     const wallet = container.querySelector('.wallet');
     await user.click(wallet as Element);
 
-    // then
     expect(screen.findByText('My Wallet')).toBeTruthy();
   });
 });
