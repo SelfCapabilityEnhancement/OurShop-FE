a9f0bf4f2e1832d66204f060f4ceb923684a464e
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-09-29 14:21:19 +0800

    feat(#3): update PurchaseConfirmation and test

--- src/components/features/purchase-confirmation/PurchaseConfirmation.test.tsx
+++ src/components/features/purchase-confirmation/PurchaseConfirmation.test.tsx
@@ -1,19 +1,41 @@
 import { render, screen } from '@testing-library/react';
 import { Container } from 'react-dom';
 import PurchaseConfirmation from '@/components/features/purchase-confirmation/PurchaseConfirmation';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Location } from 'react-router-dom';
+import * as ReactRouter from 'react-router';
+
+import userEvent from '@testing-library/user-event';
+import { tempProducts } from '@/assets/mockData';
 
 
 describe('purchase confirmation', () => {
   let container: Container;
+  const user = userEvent.setup();
+  const mockLocation: Location = {
+    key: 'default',
+    pathname: '',
+    search: '',
+    hash: '',
+    state: tempProducts,
+  };
 
   beforeEach(() => {
+    jest.spyOn(ReactRouter, 'useLocation').mockReturnValue(mockLocation);
     container = render(<PurchaseConfirmation />, { wrapper: BrowserRouter }).container;
   });
+
   it('should display purchase info', () => {
     expect(screen.getByText('Purchase Confirmation')).toBeTruthy();
     expect(screen.getByText('My Tokens:')).toBeTruthy();
     expect(screen.getByText('Cost of Tokens:')).toBeTruthy();
     expect(container.querySelectorAll('.button')).toHaveLength(2);
+  });
+
+  it('should navigate to shopping cart when click cancel', async () => {
+    const cancelButton = container.querySelector('.cancel.button');
+
+    await user.click(cancelButton as Element);
+
+    expect(screen.findByTestId('shopping-cart')).toBeTruthy();
   });
 });
--- src/components/features/purchase-confirmation/PurchaseConfirmation.tsx
+++ src/components/features/purchase-confirmation/PurchaseConfirmation.tsx
@@ -1,10 +1,15 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Product } from '@/components/common/CustomeTypes';
 
 export default function PurchaseConfirmation() {
   const navigate = useNavigate();
+  const { state }: { state: Array<Product> } = useLocation();
 
-  const handleClick = () => {
-    navigate('/home');
+  const handleClickCancel = () => {
+    navigate('/shopping-cart');
+  };
+  const handleClickBuy = () => {
+    navigate('/shopping-cart');
   };
 
   return (
@@ -12,42 +17,46 @@ export default function PurchaseConfirmation() {
       <h1 className="wallet-header text-center text-3xl mb-10">Purchase Confirmation</h1>
 
       <ul className="flex-1 flex flex-col">
-        <li className="product border-gray-400 my-5 h-10">
-          <div className="flex flex-1 flex-row items-center items-center p-4">
-            <div className="justify-center items-center mr-4">
-              <img alt="product"
-                   src="src/assets/images/product/product1.png"
-                   className="mx-auto object-cover rounded-lg h-20 w-24" />
-            </div>
-            <div className="flex-1 text-gray-600 text-2xl font-medium">
-              Product 1
-            </div>
-            <div className="pl-1 mr-16">
-              <div className="text-2xl font-normal">
-                Number: 2
+        {
+          state.map(({name, token, count}, index) => (
+            <li key={`product-${index}`} className="product border-gray-400 my-7 h-10">
+              <div className="flex flex-1 flex-row items-center items-center p-4">
+                <div className="justify-center items-center mr-4">
+                  <img alt="product"
+                       src="src/assets/images/product/product1.png"
+                       className="mx-auto object-cover rounded-lg h-20 w-24" />
+                </div>
+                <div className="flex-1 text-gray-600 text-2xl font-medium">
+                  {name}
+                </div>
+                <div className="pl-1 mr-16">
+                  <div className="text-2xl font-normal">
+                    Number: {count}
+                  </div>
+                  <div className="text-xl font-normal">
+                    Token: {token}
+                  </div>
+                </div>
               </div>
-              <div className="text-xl font-normal">
-                Token: 3
-              </div>
-            </div>
-          </div>
-        </li>
+            </li>
+          ))
+        }
       </ul>
-      <div className="grid w-1/2 ml-auto mb-10 grid-cols-2 grid-rows-2">
-        <div className="text-right">My Tokens:</div>
-        <div className="ml-5">999</div>
-        <div className="text-right">Cost of Tokens:</div>
-        <div className="ml-5">235</div>
+      <div className="grid w-1/2 ml-auto mb-10 grid-cols-2 gap-y-4">
+        <div className="text-right text-2xl">My Tokens:</div>
+        <div className="ml-5 text-2xl text-purple-500">999</div>
+        <div className="text-right text-2xl">Cost of Tokens:</div>
+        <div className="ml-5 text-2xl text-red-600">235</div>
       </div>
       <div className="mt-auto mb-5 flex justify-around">
         <button type='button'
-                onClick={handleClick}
-                className='button w-1/4 px-3 py-3 h-12 text-lg text-white font-semibold rounded-lg bg-gray-400 hover:bg-gray-600 '>
+                onClick={handleClickCancel}
+                className='button cancel w-1/4 px-3 py-3 h-12 text-lg text-white font-semibold rounded-lg bg-gray-400 hover:bg-gray-600 '>
           Cancel
         </button>
         <button type='button'
-                onClick={handleClick}
-                className='button w-1/4 px-3 py-3 h-12 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 '>
+                onClick={handleClickBuy}
+                className='button buy w-1/4 px-3 py-3 h-12 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 '>
           Buy
         </button>
       </div>
