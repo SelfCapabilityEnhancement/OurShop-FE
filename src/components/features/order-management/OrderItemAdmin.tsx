bbe3b97fe4162306f90f740b6098cd1b0622a933
linxian.wu <linxian.wu@thoughtworks.com>
2022-09-30 09:51:04 +0800

    feat(#5): show orderItem for Admin and test

---
+++ src/components/features/order-management/OrderItemAdmin.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function OrderItemAdmin(props: { order: any }) {
+  const order = props.order;
+  return (
+    <div className="flex flex-row transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white rounded-md items-center p-4 relative">
+      <div className="w-20 h-16 flex-initial mx-5">
+        <img
+          alt="profile"
+          src={`src/assets/images/product/product${order.id}.png`}
+          className="mx-auto object-cover rounded-lg"
+          data-testid="product-picture"
+        />
+      </div>
+      <span className="font-medium flex-auto mx-5" data-testid="product-name">
+        {order.productName}
+      </span>
+      <div className="absolute right-10">
+        <span data-testid="number-title">number: </span>
+        <span data-testid="purchase-number">{order.purchaseNumber}</span>
+      </div>
+    </div>
+  );
+}
