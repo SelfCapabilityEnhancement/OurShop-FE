85b7b3691350ca4866522fadb81be8a599b1cd04
linxian.wu <linxian.wu@thoughtworks.com>
2022-10-09 11:05:53 +0800

    refactor(6): extract TextSubmission and test

---
+++ src/components/common/TextSubmission/TextSubmission.test.tsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react';
+import { TextSubmission } from '@/components/common/TextSubmission/TextSubmission';
+import { Container } from 'react-dom';
+import MyAddress from '@/components/features/my-address/MyAddress';
+import { BrowserRouter } from 'react-router-dom';
+
+describe('TextSubmission', () => {
+  let container: Container;
+  beforeEach(() => {
+    container = render(<TextSubmission value='' handleCancel={() => {
+    }} handleEdit={() => {
+    }} handleSave={() => {
+    }} />, { wrapper: BrowserRouter }).container;
+  });
+  test('should show textArea, save button and cancel button', () => {
+    const save = container.querySelector('.save.button');
+    const cancel = container.querySelector('.cancel.button');
+    const addressInput = container.querySelector('.address-input');
+    expect(save).toBeTruthy();
+    expect(cancel).toBeTruthy();
+    expect(addressInput).toBeTruthy();
+  });
+});
--- src/components/features/my-address/MyAddress.tsx
+++ src/components/features/my-address/MyAddress.tsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import { Listbox, Transition } from '@headlessui/react';
 import React, { Fragment, useState } from 'react';
+import { TextSubmission } from '@/components/common/TextSubmission/TextSubmission';
 
 const baseCites = [
   { id: 1, name: 'Wuhan' },
@@ -8,34 +9,6 @@ const baseCites = [
   { id: 3, name: 'Shanghai' },
   { id: 4, name: 'Chengdu' },
 ];
-
-function TextSubmission(props: { value: string, handleEdit: Function, handleCancel: Function, handleSave: Function }) {
-  return (
-    <div className='flex flex-col justify-items-center h-[480px]'>
-        <textarea
-          className='address-input appearance-none border border-gray-300 mx-8 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
-          placeholder='Enter your shipping address'
-          name='shippingAddress'
-          value={props.value}
-          onChange={(event) => props.handleEdit(event)}
-          rows={5}
-          cols={30}
-        ></textarea>
-      <div className='mb-5 flex justify-around mt-60'>
-        <button type='button'
-                onClick={()=>props.handleCancel()}
-                className='button cancel w-1/4 p-3 h-12 text-lg text-white font-semibold rounded-lg bg-gray-400 hover:bg-gray-600 '>
-          Cancel
-        </button>
-        <button type='button'
-                onClick={()=>props.handleSave()}
-                className='button save w-1/4 p-3 h-12 text-lg text-white font-semibold rounded-lg bg-violet-500 hover:bg-violet-700 '>
-          Save
-        </button>
-      </div>
-    </div>
-  );
-}
 
 export default function MyAddress() {
   const navigate = useNavigate();
