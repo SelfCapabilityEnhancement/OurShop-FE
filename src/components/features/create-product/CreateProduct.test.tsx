5c86732fb7b80f1a93e321dbf77c8f2819769c09
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-09 17:40:00 +0800

    feat: update CreateProduct content

--- src/components/features/create-product/CreateProduct.test.tsx
+++ src/components/features/create-product/CreateProduct.test.tsx
@@ -1,3 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import { Container } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import CreateProduct from '@/components/features/create-product/CreateProduct';
+import userEvent from '@testing-library/user-event';
+
+
+
 describe('Create product test', () => {
-  it('', () => {});
+  let container: Container;
+  const user = userEvent.setup();
+
+  beforeEach(() => {
+    container = render(<CreateProduct />, {wrapper: BrowserRouter}).container;
+  });
+
+  it('should display create product form', () => {
+    expect(screen.getByText('product name')).toBeInTheDocument();
+    expect(screen.getByText('price in USD')).toBeInTheDocument();
+    expect(screen.getByText('price in token')).toBeInTheDocument();
+    expect(screen.getByText('product description')).toBeInTheDocument();
+    expect(screen.getByText('picture')).toBeInTheDocument();
+    expect(container.querySelector('.create.button')).toBeInTheDocument();
+  });
 });
--- src/components/features/create-product/CreateProduct.tsx
+++ src/components/features/create-product/CreateProduct.tsx
@@ -1,7 +1,58 @@
-import React from 'react';
-
 function CreateProduct() {
-  return <div>create product page</div>;
+  return (
+    <form className='m-8'>
+      <div className='mb-6 grid grid-cols-2 gap-y-4 text-xl font-normal w-96'>
+        <label htmlFor='name'
+               className='mr-5 w-30'>
+          <span className='text-red-500 pr-1'>*</span>product name
+        </label>
+        <input type='text'
+               id='name'
+               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+               required />
+
+        <label htmlFor='usd'
+               className='mr-5'>
+          <span className='text-red-500 pr-1'>*</span>price in USD
+        </label>
+        <input type='number'
+               id='usd'
+               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+               required />
+
+        <label htmlFor='token'
+               className='mr-5'>
+          <span className='text-red-500 pr-1'>*</span>price in token
+        </label>
+        <input type='number'
+               id='token'
+               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+               required />
+
+        <label htmlFor='description'
+               className='mr-5 col-span-2'>
+          <span className='text-red-500 pr-1'>*</span>product description
+        </label>
+        <input type='text'
+               id='description'
+               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+               required />
+
+        <label htmlFor='description'
+               className='mr-5 col-span-2'>
+          <span className='text-red-500 pr-1'>*</span>picture
+        </label>
+        <input type='text'
+               id='description'
+               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+               required />
+      </div>
+      <button type='submit'
+              className='create button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>
+        Create Product
+      </button>
+    </form>
+  );
 }
 
 export default CreateProduct;
