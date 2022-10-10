5c6a1342189320d2eee1b28a41493cec0bca9938
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-10 11:00:52 +0800

    refactor: update field in Product to align database

--- src/assets/mockData.ts
+++ src/assets/mockData.ts
@@ -1,9 +1,9 @@
 import { Product } from '@/components/common/CustomeTypes';
 
 const tempProducts: Array<Product> = [
-  {name: 'Product1', token: 5, usd: 0, description: ''},
-  {name: 'Product2', token: 3, usd: 0, description: ''},
-  {name: 'Product3', token: 2, usd: 0, description: ''},
+  {id: '1', name: 'Product1', token: 5, usd: 0, description: '', images: [], sku: 2},
+  {id: '2', name: 'Product2', token: 3, usd: 0, description: '', images: [], sku: 3},
+  {id: '3', name: 'Product3', token: 2, usd: 0, description: '', images: [], sku: 5},
 ];
 
 const count = [2, 1, 3];
--- src/components/common/CustomeTypes.ts
+++ src/components/common/CustomeTypes.ts
@@ -1,6 +1,9 @@
 export interface Product {
+  id: string
   name: string
   token: number
   usd: number
+  sku: number
   description: string
+  images: string[]
 }
