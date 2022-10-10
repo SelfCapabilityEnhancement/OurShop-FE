c759c897b5caa64811d5fe0272f16617b2afde4e
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-10 14:51:00 +0800

    feat: implement onChange func

--- src/components/features/create-product/CreateProduct.tsx
+++ src/components/features/create-product/CreateProduct.tsx
@@ -5,17 +5,17 @@ import { Product } from '@/components/common/CustomeTypes';
 const emptyProduct: Product = {
   id: '',
   name: '',
-  token: -1,
-  usd: -1,
+  token: undefined,
+  usd: undefined,
   description: '',
-  sku: -1,
+  sku: undefined,
   images: [],
 };
 
 const basicForm = [
-  { id: 'name', label: 'product name' },
-  { id: 'usd', label: 'price in USD' },
-  { id: 'token', label: 'price in token' }];
+  { id: 'name', label: 'product name', type: 'string' },
+  { id: 'usd', label: 'price in USD', type: 'number' },
+  { id: 'token', label: 'price in token', type: 'number' }];
 
 function CreateProduct() {
   const [product, setProduct] = useState<Product>(emptyProduct);
@@ -33,19 +33,43 @@ function CreateProduct() {
     }
   };
 
+  const handleInputField = (event: React.ChangeEvent<HTMLInputElement|HTMLTextAreaElement>, field: string) => {
+    const value = event.target.value;
+    const tmp = { ...product };
+    switch (field) {
+      case 'name':
+        tmp.name = value;
+        break;
+      case 'usd':
+        tmp.usd = Number(value);
+        break;
+      case 'token':
+        tmp.token = Number(value);
+        break;
+      case 'description':
+        tmp.description = value;
+    }
+
+    setProduct(tmp);
+  };
+
+  // @ts-ignore
+  // @ts-ignore
   return (
     <form className='m-8'>
       <div className='mb-6 grid grid-cols-2 gap-y-4 text-xl font-normal w-96'>
-        {basicForm.map(({ id, label }) => (
-          <>
+        {basicForm.map(({ id, label, type }) => (
+          <div key={id} className='col-span-2 grid grid-cols-2 gap-y-4'>
             <label htmlFor={id}
                    className='mr-5 w-30'>
               <span className='text-red-500 pr-1'>*</span>{label}
             </label>
-            <input type='text'
+            <input type={type === 'string' ? 'text' : 'number'}
                    className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+                   onChange={(event) => handleInputField(event, id)}
+                   value={product[id as keyof Product]}
                    id={id} />
-          </>
+          </div>
         ))}
 
         <label htmlFor='description'
@@ -55,6 +79,7 @@ function CreateProduct() {
         <textarea
           className='col-span-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
           value={product.description}
+          onChange={(event) => handleInputField(event, 'description')}
           id='description' />
 
         <ImageUploader images={product.images} handleNewImage={handleNewImage} />
