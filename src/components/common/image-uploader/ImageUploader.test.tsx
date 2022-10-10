384ef5d9edea166ca9d3124d8e91b5f709432bdc
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-10 11:04:02 +0800

    feat: implement ImageUploader

---
+++ src/components/common/image-uploader/ImageUploader.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import { Container } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import ImageUploader from '@/components/common/image-uploader/ImageUploader';
+
+
+describe('Image Uploader', () => {
+  let container: Container;
+
+  beforeEach(() => {
+    const mockHandleNewImage = jest.fn(() => {});
+    container = render(<ImageUploader images={[]} handleNewImage={mockHandleNewImage} />, {wrapper: BrowserRouter}).container;
+  });
+
+  it('should display image uploader', () => {
+    expect(screen.getByText('picture')).toBeInTheDocument();
+    expect(container.querySelector('.new-image')).toBeInTheDocument();
+  });
+});
--- src/components/features/create-product/CreateProduct.tsx
+++ src/components/features/create-product/CreateProduct.tsx
@@ -1,4 +1,20 @@
+import ImageUploader from '@/components/common/image-uploader/ImageUploader';
+import React, { useState } from 'react';
+import { Product } from '@/components/common/CustomeTypes';
+
 function CreateProduct() {
+  const [product, setProduct] = useState<Product>({id: '', name: '', token: -1, usd: -1, description: '', sku: -1, images: []});
+
+  const handleNewImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files !== null && files.length > 0) {
+      setProduct((prevState) => {
+          return {...prevState, images: [...prevState.images, URL.createObjectURL(files[0])]};
+        }
+      );
+    }
+  };
+
   return (
     <form className='m-8'>
       <div className='mb-6 grid grid-cols-2 gap-y-4 text-xl font-normal w-96'>
@@ -7,45 +23,34 @@ function CreateProduct() {
           <span className='text-red-500 pr-1'>*</span>product name
         </label>
         <input type='text'
-               id='name'
                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-               required />
+               id='name' />
 
         <label htmlFor='usd'
                className='mr-5'>
           <span className='text-red-500 pr-1'>*</span>price in USD
         </label>
         <input type='number'
-               id='usd'
                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-               required />
+               id='usd' />
 
         <label htmlFor='token'
                className='mr-5'>
           <span className='text-red-500 pr-1'>*</span>price in token
         </label>
         <input type='number'
-               id='token'
                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-               required />
+               id='token' />
 
         <label htmlFor='description'
                className='mr-5 col-span-2'>
           <span className='text-red-500 pr-1'>*</span>product description
         </label>
         <input type='text'
-               id='description'
                className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-               required />
+               id='description' />
 
-        <label htmlFor='description'
-               className='mr-5 col-span-2'>
-          <span className='text-red-500 pr-1'>*</span>picture
-        </label>
-        <input type='text'
-               id='description'
-               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-               required />
+        <ImageUploader images={product.images} handleNewImage={handleNewImage} />
       </div>
       <button type='submit'
               className='create button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>
