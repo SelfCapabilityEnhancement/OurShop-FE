d8d3b532a97d20d0af133d88672501b68c8085eb
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-11 11:08:26 +0800

    feat: update Homepage content

--- src/components/features/home-page/HomePage.tsx
+++ src/components/features/home-page/HomePage.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import { getProducts } from '@/assets/mockData';
+import productImage from 'images/product/product1.png';
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const products = getProducts();
+
+  return (
+    <div className='grid grid-flow-col auto-cols-min mt-10'>
+      {products.map((product) => (
+        <div key={product.id}
+             className='product w-60 h-48 mx-5 flex flex-col bg-gray-100'>
+          <img src={productImage} alt='product' />
+          <div className='bg-gray-100'>
+            <div className='text-xl font-normal'>{product.name}</div>
+            <div className='text-xl font-normal text-red-600'>
+              {`$${product.usd} or ${product.token} Token`}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
