79750996350c14877114bd170bfb28117f093105
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-11 09:26:20 +0800

    refactor: extract common util func classNames

--- src/components/common/dropdown/Dropdown.tsx
+++ src/components/common/dropdown/Dropdown.tsx
@@ -2,10 +2,7 @@
 import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import { ChevronDownIcon } from '@heroicons/react/solid';
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(' ');
-}
+import { classNames } from '@/components/common/utils';
 
 export default function Example() {
   return (
