c7aadc2bd536c3e03c67db44d475e5027084c2a1
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-11 10:25:58 +0800

    feat: add tabs in CreateProduct

--- src/components/common/banner/Banner.tsx
+++ src/components/common/banner/Banner.tsx
@@ -3,10 +3,10 @@ import { Fragment } from 'react';
 
 export default function Banner({
                                  visible,
-                                 result,
+                                 success,
                                  successMsg,
                                  failMsg,
-                               }: { visible: boolean, result: boolean, successMsg: string, failMsg: string }) {
+                               }: { visible: boolean, success: boolean, successMsg: string, failMsg: string }) {
   return (
     <Transition appear show={visible} as={Fragment}>
       <Dialog as='div' className='relative z-10' onClose={() => {
@@ -34,19 +34,19 @@ export default function Banner({
               leaveFrom='opacity-100 scale-100'
               leaveTo='opacity-0 scale-95'
             >
-              <Dialog.Panel className={`${result ? 'bg-green-200' : 'bg-red-200'} transform overflow-hidden rounded-md p-3 text-left align-middle shadow-xl transition-all`}>
+              <Dialog.Panel className={`${success ? 'bg-green-200' : 'bg-red-200'} transform overflow-hidden rounded-md p-3 text-left align-middle shadow-xl transition-all`}>
                 <div className='flex flex-row items-center text-sm pr-5'>
                   <svg xmlns='http://www.w3.org/2000/svg'
                        fill='none'
                        viewBox='0 0 24 24'
                        strokeWidth={1.5}
                        stroke='currentColor'
-                       className={`w-6 h-6 ${result ? 'text-green-600' : 'text-red-600'} mr-2`}>
+                       className={`w-6 h-6 ${success ? 'text-green-600' : 'text-red-600'} mr-2`}>
                     <path strokeLinecap='round'
                           strokeLinejoin='round'
-                          d={result ? 'M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' : 'M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'} />
+                          d={success ? 'M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z' : 'M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'} />
                   </svg>
-                  {result ? successMsg : failMsg}
+                  {success ? successMsg : failMsg}
                 </div>
               </Dialog.Panel>
             </Transition.Child>
--- src/components/features/create-product/CreateProduct.tsx
+++ src/components/features/create-product/CreateProduct.tsx
@@ -1,7 +1,9 @@
 import ImageUploader from '@/components/common/image-uploader/ImageUploader';
 import React, { useState } from 'react';
+import { Tab } from '@headlessui/react';
 import { Product } from '@/components/common/CustomeTypes';
 import Banner from '@/components/common/banner/Banner';
+import {classNames} from '@/components/common/utils';
 
 const successMsg = 'The product was created successfully!';
 const failMsg = 'all required field must be filled';
@@ -20,6 +22,12 @@ const basicForm = [
   { id: 'name', label: 'product name', type: 'string' },
   { id: 'usd', label: 'price in USD', type: 'number' },
   { id: 'token', label: 'price in token', type: 'number' }];
+
+const tabs = [
+  { id: 'productInfo', name: 'Product Information' },
+  { id: 'logisticInfo', name: 'Logistic Information' },
+  { id: 'approvalFlow', name: 'approval flow' },
+];
 
 function CreateProduct() {
   const [product, setProduct] = useState<Product>(emptyProduct);
@@ -75,44 +83,71 @@ function CreateProduct() {
     setTimeout(() => SetShowBanner(false), 1500);
 
     if (validateForm()) {
+      setTimeout(() => setProduct(emptyProduct), 1500);
       // Post
     }
   };
 
   return (
-    <div className='m-8'>
-      <Banner visible={showBanner} result={validateForm()} successMsg={successMsg} failMsg={failMsg} />
-      <form className='mb-6 grid grid-cols-2 gap-y-4 text-xl font-normal w-96'>
-          {basicForm.map(({ id, label, type }) => (
-            <div key={id} className='col-span-2 grid grid-cols-2 gap-y-4'>
-              <label htmlFor={id}
-                     className='mr-5 w-30'>
-                <span className='text-red-500 pr-1'>*</span>{label}
-              </label>
-              <input type={type === 'string' ? 'text' : 'number'}
-                     className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-                     onChange={(event) => handleInputField(event, id)}
-                     value={ product[id as keyof Product] === 0 ? '' : product[id as keyof Product]}
-                     id={id} />
-            </div>
+    <div className='w-full max-w-full p-3'>
+      <Tab.Group>
+        <Tab.List className='flex space-x-10 p-1'>
+          {tabs.map((tab) => (
+            <Tab key={tab.id}
+                 className={({selected}) => classNames(
+                   'w-52 rounded-lg text-xl font-normal',
+                   selected
+                     ? `${tab.id} text-purple-500 underline underline-offset-8`
+                     : 'text-gray-800'
+                 )}>
+              {tab.name}
+            </Tab>
           ))}
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <div className='m-8'>
+              <Banner visible={showBanner}
+                      success={validateForm()}
+                      successMsg={successMsg}
+                      failMsg={failMsg} />
+              <form className='mb-6 grid grid-cols-2 gap-y-4 text-xl font-normal w-96'>
+                {basicForm.map(({ id, label, type }) => (
+                  <div key={id} className='col-span-2 grid grid-cols-2 gap-y-4'>
+                    <label htmlFor={id}
+                           className='mr-5 w-30'>
+                      <span className='text-red-500 pr-1'>*</span>{label}
+                    </label>
+                    <input type={type === 'string' ? 'text' : 'number'}
+                           className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 text-center rounded focus:outline-none focus:ring'
+                           onChange={(event) => handleInputField(event, id)}
+                           value={product[id as keyof Product] === 0 ? '' : product[id as keyof Product]}
+                           id={id} />
+                  </div>
+                ))}
 
-          <label htmlFor='description'
-                 className='mr-5 col-span-2'>
-            <span className='text-red-500 pr-1'>*</span>product description
-          </label>
-          <textarea
-            className='col-span-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
-            value={product.description}
-            onChange={(event) => handleInputField(event, 'description')}
-            id='description' />
+                <label htmlFor='description'
+                       className='mr-5 col-span-2'>
+                  <span className='text-red-500 pr-1'>*</span>product description
+                </label>
+                <textarea
+                  className='col-span-2 shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-base p-2 rounded focus:outline-none focus:ring'
+                  value={product.description}
+                  onChange={(event) => handleInputField(event, 'description')}
+                  id='description' />
 
-          <ImageUploader images={product.images} handleNewImage={handleNewImage} />
-      </form>
-      <button onClick={handleSubmit}
-              className='create button text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'>
-        Create Product
-      </button>
+                <ImageUploader images={product.images} handleNewImage={handleNewImage} />
+              </form>
+              <button onClick={handleSubmit}
+                      className='create button text-white bg-violet-500 hover:bg-violet-700 font-medium rounded-lg text-lg w-64 px-5 py-2.5 text-center'>
+                Create Product
+              </button>
+            </div>
+          </Tab.Panel>
+          <Tab.Panel>Logistic Information</Tab.Panel>
+          <Tab.Panel>approval flow</Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 }
