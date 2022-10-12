af3cd478157c53d48df38ec494ff360f3b514570
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-12 15:21:42 +0800

    feat: add base64 func

--- src/components/common/utils.ts
+++ src/components/common/utils.ts
@@ -1,3 +1,11 @@
 export function classNames(...classes: any) {
   return classes.filter(Boolean).join(' ');
 }
+
+export const toBase64 = (file: File) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
