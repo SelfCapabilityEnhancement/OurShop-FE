e295a0740cb9a10516265cca269aa4c3a702c651
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-12 16:00:37 +0800

    build: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,27 +3,36 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: resolve(__dirname, 'src'),
-      },
-      {
-        find: 'images',
-        replacement: resolve(__dirname, 'src/assets/images'),
-      },
-    ],
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://ourshop.azurewebsites.net/api/v1',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ command }) => {
+  let targetURL;
+  if (command === 'serve') {
+    targetURL = 'http://127.0.0.1:8080';
+  } else if (command === 'build') {
+    targetURL = 'https://ourshop.azurewebsites.net/api/v1';
+  }
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: resolve(__dirname, 'src'),
+        },
+        {
+          find: 'images',
+          replacement: resolve(__dirname, 'src/assets/images'),
+        },
+      ],
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: targetURL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
       },
     },
-  },
+  };
 });
