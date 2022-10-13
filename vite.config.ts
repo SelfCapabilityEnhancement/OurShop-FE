6fe5e2ae742e9796fe625db5a669c3464a4ac160
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-13 09:16:17 +0800

    build: update default value of targetURL

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,9 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
-  let targetURL;
+  let targetURL = 'https://ourshop.azurewebsites.net/api/v1';
   if (command === 'serve') {
-    targetURL = 'http://127.0.0.1:8080';
-  } else if (command === 'build') {
-    targetURL = 'https://ourshop.azurewebsites.net/api/v1';
+    targetURL = 'http://127.0.0.1:8080/api/v1';
   }
 
   return {
