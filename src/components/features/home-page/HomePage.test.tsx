2528442dd84d4b5e378453da014554d22880a5d6
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-13 16:31:30 +0800

    feat: add navigate to detail

--- src/components/features/home-page/HomePage.test.tsx
+++ src/components/features/home-page/HomePage.test.tsx
@@ -1,24 +1,36 @@
-import { render, waitFor } from '@testing-library/react';
-import { Container } from 'react-dom';
+import { render, screen, waitFor } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import HomePage from '@/components/features/home-page/HomePage';
 import { tempProducts } from '@/mocks/mockData';
 import { http } from '@/service';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
 
 describe('HomePage', () => {
-  let container: Container;
+  const user = userEvent.setup();
 
   beforeEach(() => {
     jest.spyOn(http, 'get').mockResolvedValue({ data: tempProducts });
-    container = render(<HomePage />, { wrapper: BrowserRouter }).container;
+    render(<HomePage />, { wrapper: BrowserRouter });
   });
 
-  it('should display products', () => {
-    const products = container.querySelectorAll('.product');
+  it('should display products', async () => {
+    const products = await screen.findAllByRole('img');
 
-    waitFor(() => {
-      expect(products).toHaveLength(tempProducts.length);
+    expect(await screen.findByText(tempProducts[0].name)).toBeInTheDocument();
+    expect(products).toHaveLength(tempProducts.length);
+  });
+
+  it('should navigate to detail page when click product', () => {
+    const { name, description } = tempProducts[0];
+
+    act(async () => {
+      await user.click(await screen.findByText(name));
+    });
+
+    waitFor(async () => {
+      expect(await screen.findByText(description)).toBeInTheDocument();
     });
   });
 });
