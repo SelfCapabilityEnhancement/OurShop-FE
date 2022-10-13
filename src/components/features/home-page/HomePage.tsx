e47e45b7e9b6905f1d188ee705fa1d552d91b398
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-13 16:31:30 +0800

    feat: add navigate to detail

--- src/components/features/home-page/HomePage.tsx
+++ src/components/features/home-page/HomePage.tsx
@@ -2,8 +2,10 @@ import productImage from 'images/product/product1.png';
 import { useEffect, useState } from 'react';
 import { Product } from '@/components/common/CustomeTypes';
 import { http } from '@/service';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
   const [products, setProducts] = useState<Product[]>([]);
   useEffect(() => {
     const getProducts = async () => {
@@ -13,10 +15,15 @@ export default function HomePage() {
     getProducts().then();
   }, []);
 
+  const handleClick = (product: Product) => {
+    navigate('/detail', {state: { product }});
+  };
+
   return (
     <div className='grid grid-cols-fluid justify-between my-10'>
       {products.map((product) => (
         <div key={product.id}
+             onClick={() => handleClick(product)}
              className='product w-60 h-48 mb-20 mx-auto flex flex-col bg-gray-100'>
           <img src={productImage} alt='product' />
           <div className='bg-gray-100'>
