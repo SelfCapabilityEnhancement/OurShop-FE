513b85497c6c3e808d5cb2f8825772fe55ad92ed
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-10-13 09:18:34 +0800

    feat: replace mock getProduct with http.get

--- src/components/features/home-page/HomePage.tsx
+++ src/components/features/home-page/HomePage.tsx
@@ -1,14 +1,23 @@
-import { getProducts } from '@/assets/mockData';
 import productImage from 'images/product/product1.png';
+import { useEffect, useState } from 'react';
+import { Product } from '@/components/common/CustomeTypes';
+import { http } from '@/service';
 
 export default function HomePage() {
-  const products = getProducts();
+  const [products, setProducts] = useState<Product[]>([]);
+  useEffect(() => {
+    const getProducts = async () => {
+      await http.get('/product').then(({ data }) => setProducts(data));
+    };
+
+    getProducts().then();
+  }, []);
 
   return (
-    <div className='grid grid-flow-col auto-cols-min mt-10'>
+    <div className='grid grid-cols-fluid justify-between my-10'>
       {products.map((product) => (
         <div key={product.id}
-             className='product w-60 h-48 mx-5 flex flex-col bg-gray-100'>
+             className='product w-60 h-48 mb-20 mx-auto flex flex-col bg-gray-100'>
           <img src={productImage} alt='product' />
           <div className='bg-gray-100'>
             <div className='text-xl font-normal'>{product.name}</div>
