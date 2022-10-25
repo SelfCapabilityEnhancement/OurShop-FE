08b4e7ac1f3aabfa5b75ecb3a4b4c51f708c05e8
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-10-25 13:56:52 +0800

    chore: justify style of view detail

--- src/components/features/order-management/OrderDetailWindow.tsx
+++ src/components/features/order-management/OrderDetailWindow.tsx
@@ -41,27 +41,46 @@ export default function OrderDetailWindow(props: {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
+                    className="text-lg font-medium leading-6 text-gray-900 grid grid-cols-3"
                   >
-                    Order Detail
+                    <div></div>
+                    <div className="justify-self-center">Order Detail</div>
+                    <div className="justify-self-end">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </div>
                   </Dialog.Title>
                   <div className="mt-2 flex">
                     <img
                       alt="profile"
                       src={selectedOrdersItemAdmin.product.images.split(',')[0]}
-                      className="mx-auto object-cover rounded-lg"
+                      className="ml-2 object-cover rounded-lg w-16 h-16 border-pink-500 clear-left"
                       data-testid="product-picture"
                     />
-                    <p className="text-sm text-gray-500">
+                    <p className="text-base text-black ml-10 my-4">
                       {selectedOrdersItemAdmin.product.name}
                     </p>
                   </div>
                   <div>
-                    <p>Order Number: {selectedOrdersItemAdmin.productNumAll}</p>
+                    <p className="text-base ml-2">
+                      Order Number: {selectedOrdersItemAdmin.productNumAll}
+                    </p>
                   </div>
                   <div>
-                    <p>Buyer Information</p>
-                    <p className="description bg-slate-100 rounded-xl h-[210px] py-3 px-3 text-l">
+                    <p className="ml-2">Buyer Information</p>
+                    <p className="description bg-slate-100 rounded-xl h-[210px] py-3 px-3 text-base">
                       {selectedOrdersItemAdmin.ordersList.map(
                         (order) =>
                           // FIXME buyer information
@@ -70,17 +89,16 @@ export default function OrderDetailWindow(props: {
                     </p>
                   </div>
 
-                  <div className="mt-4">
+                  <div className="mt-4 flex justify-evenly">
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="inline-flex rounded-md border border-transparent bg-indigo-400 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeDetailWindow}
                     >
                       Export to Excel
                     </button>
                     <button
-                      type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      className="inline-flex rounded-md border border-transparent bg-fuchsia-400 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                       onClick={closeDetailWindow}
                     >
                       Order is Made
