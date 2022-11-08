41ef945bb768d53224b8e750262ab8e2ac3d764a
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-11-08 14:48:51 +0800

    feat:  header underline does not disappear when clicked elsewhere

--- src/components/features/header/Header.tsx
+++ src/components/features/header/Header.tsx
@@ -1,9 +1,10 @@
-import { NavLink, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Profile from '../profile/Profile';
 import { useEffect, useState } from 'react';
 import { ShoppingCartItem } from '@/components/common/CustomeTypes';
 import { getCurrentUser, getShoppingCarts } from '@/service';
 import { classNames } from '@/utils';
+import { Tab } from '@headlessui/react';
 
 export default function Header() {
   const navigate = useNavigate();
@@ -22,6 +23,29 @@ export default function Header() {
     navigate('/home');
   };
 
+  const headers = [
+    { id: 'orderManagement', name: 'Order Management' },
+    { id: 'createProduct', name: 'Create Product' },
+    { id: 'shoppingCart', name: 'Shopping Cart' },
+    { id: 'myOrder', name: 'My Order' },
+  ];
+
+  const [selectedHeader, setSelectedHeader] = useState(0);
+
+  const showHeader = (headerId: string) => {
+    if (headerId === 'orderManagement') {
+      navigate('/order-management');
+    } else if (headerId === 'createProduct') {
+      navigate('/create-product');
+    } else if (headerId === 'shoppingCart') {
+      navigate('/shopping-cart');
+    } else if (headerId === 'myOrder') {
+      navigate('/my-order');
+    } else {
+      navigate('/order-management');
+    }
+  };
+
   return (
     <div className="flex justify-between items-center shadow-md h-[72px]">
       <div className="flex items-center basis-1/3 ml-8">
@@ -36,47 +60,42 @@ export default function Header() {
         </span>
       </div>
       <div className="flex items-center basis-1/2">
-        <div className="nav-list flex justify-around flex-1">
-          <NavLink
-            to="order-management"
-            className="p-4 text-center border-b-2 border-white
-            hover:text-gray-600 hover:border-gray-300
-            focus:text-pink-500 focus:border-pink-500 "
+        <Tab.Group
+          manual
+          selectedIndex={selectedHeader}
+          onChange={setSelectedHeader}
+        >
+          <Tab.List
+            className="flex w-11/12 border-b-2 border-white"
+            data-testid="header"
           >
-            Order Management
-          </NavLink>
-          <NavLink
-            to="create-product"
-            className="p-4 text-center border-b-2 border-white
-            hover:text-gray-600 hover:border-gray-300
-            focus:text-pink-500 focus:border-pink-500"
-          >
-            Create Product
-          </NavLink>
-          <NavLink
-            to="shopping-cart"
-            className="p-4 text-center border-b-2 border-white
-            hover:text-gray-600 hover:border-gray-300
-            focus:text-pink-500 focus:border-pink-500"
-          >
-            Shopping Cart
-            <span
-              data-testid="redDot"
-              className={classNames(
-                'inline-block w-2 h-2 bg-red-600 rounded-full mb-2 ml-1',
-                shoppingCartItems.length > 0 ? '' : 'hidden'
-              )}
-            ></span>
-          </NavLink>
-          <NavLink
-            to="my-order"
-            className="p-4 text-center border-b-2 border-white
-            hover:text-gray-600 hover:border-gray-300
-            focus:text-pink-500 focus:border-pink-500"
-          >
-            My Order
-          </NavLink>
-        </div>
+            {headers.map((header) => (
+              <Tab
+                key={header.id}
+                onClick={() => showHeader(header.id)}
+                className={({ selected }) =>
+                  classNames(
+                    `order-status-label ${header.id} w-40 rounded-lg text-l text-center outline-0`,
+                    selected
+                      ? 'text-pink-500 underline underline-offset-8 border-b-2 border-white'
+                      : 'text-gray-800'
+                  )
+                }
+              >
+                {header.name}
+                {header.id === 'shoppingCart' && (
+                  <span
+                    data-testid="redDot"
+                    className={classNames(
+                      'inline-block w-2 h-2 bg-red-600 rounded-full mb-2 ml-1',
+                      shoppingCartItems.length > 0 ? '' : 'hidden'
+                    )}
+                  ></span>
+                )}
+              </Tab>
+            ))}
+          </Tab.List>
+        </Tab.Group>
         <Profile></Profile>
       </div>
     </div>
