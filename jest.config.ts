306950004a99b34e3a2e825c68c1948fcfac2a44
chaoqun <110969610+chaoqunya@users.noreply.github.com>
2022-11-16 14:45:52 +0800

    ci: add test coverage constraint before push

---
+++ jest.config.ts
@@ -0,0 +1,40 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/mocks/fileMock.ts',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    'images/(.*)$': '<rootDir>/src/assets/images/$1',
+  },
+  transform: {
+    '\\.(js|ts)?$': 'babel-jest',
+  },
+  // tell jest to transform axios via babel-jest
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!axios/)'],
+
+  // test coverage
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/assets/**',
+    '!<rootDir>/src/service/*',
+    '!<rootDir>/src/routers/*',
+    '!<rootDir>/src/azure-storage-blob.ts',
+    '!<rootDir>/src/components/common/CustomTypes.ts',
+    '!<rootDir>/src/vite-env.d.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 63,
+      functions: 57,
+      lines: 72,
+      statements: 70,
+    },
+  },
+};
+
+export default config;
