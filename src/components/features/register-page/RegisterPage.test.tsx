cf541b27a7e298517ec13333d794f8c631b9d459
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-11-18 16:06:08 +0800

    feat: register page test

---
+++ src/components/features/register-page/RegisterPage.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import RegisterPage from '@/components/features/register-page/RegisterPage';
+
+describe('display register page', () => {
+  it('should display register page context', () => {
+    render(<RegisterPage />);
+    expect(screen.getByText('Welcome')).toBeInTheDocument();
+    expect(
+      screen.getByText('Try this shopping platform for TWers')
+    ).toBeInTheDocument();
+    expect(screen.getByText('Try it now.')).toBeInTheDocument();
+    expect(screen.getByText('Register with Us')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        'Hey TWer, please fill the necessary information to register'
+      )
+    ).toBeInTheDocument();
+    expect(screen.getByText('Already has an account?')).toBeInTheDocument();
+    expect(screen.getByText('Login OurShop')).toBeInTheDocument();
+    expect(
+      screen.getByRole('button', { name: /Register/ })
+    ).toBeInTheDocument();
+  });
+});
