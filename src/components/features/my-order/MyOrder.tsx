817e6b5044748f171768f06bff17fc2df4c2d4e4
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-11-21 16:17:38 +0800

    feat: add MyOrderDetailWindow

--- src/components/features/my-order/MyOrder.tsx
+++ src/components/features/my-order/MyOrder.tsx
@@ -2,9 +2,27 @@ import { useEffect, useState } from 'react';
 import OrderItem from '@/components/features/my-order/OrderItem';
 import { OrdersItem } from '@/components/common/CustomeTypes';
 import { getCurrentUser, getOrdersItemsByUserId } from '@/service';
+import MyOrderDetailWindow from '@/components/features/my-order/MyOrderDetailWindow';
 
 export default function MyOrder() {
   const [ordersItems, setOrdersItems] = useState<OrdersItem[]>([]);
+  const [showWindow, setShowWindow] = useState(false);
+  const [selectedOrdersItem, setSelectedOrdersItem] = useState<OrdersItem>({
+    productId: 0,
+    purchaseNum: 0,
+    orderId: 0,
+    status: '',
+    address: '',
+    vendorDate: '',
+    userId: 0,
+    purchaseDate: '',
+    productName: '',
+    description: '',
+    images: '',
+    username: '',
+    telephoneNum: '',
+    logisticMethod: '',
+  });
 
   useEffect(() => {
     getCurrentUser().then((user) => {
@@ -30,10 +48,20 @@ export default function MyOrder() {
             key={item.productId}
             className="order-item product border-gray-400 mb-5 h-20 "
           >
-            <OrderItem order={item} />
+            <OrderItem
+              order={item}
+              setShowWindow={setShowWindow}
+              setSelectedOrdersItem={setSelectedOrdersItem}
+            />
           </li>
         ))}
       </ul>
+
+      <MyOrderDetailWindow
+        showWindow={showWindow}
+        setShowWindow={setShowWindow}
+        selectedOrdersItem={selectedOrdersItem}
+      />
     </div>
   );
 }
