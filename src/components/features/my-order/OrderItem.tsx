a05503ee5dc1dcce14490b601b90390751173b95
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-11-21 16:07:13 +0800

    feat: add view detail in my order

--- src/components/features/my-order/OrderItem.tsx
+++ src/components/features/my-order/OrderItem.tsx
@@ -1,8 +1,19 @@
 import { OrdersItem } from '@/components/common/CustomeTypes';
+import React from 'react';
 
-export default function OrderItem(props: { order: OrdersItem }) {
+export default function OrderItem(props: {
+  order: OrdersItem;
+  setShowWindow: React.Dispatch<React.SetStateAction<boolean>>;
+  setSelectedOrdersItem: React.Dispatch<React.SetStateAction<OrdersItem>>;
+}) {
   const order = props.order;
-  const date = new Date(order.purchaseDate);
+  const date = order.purchaseDate;
+
+  function myOrderDetailWindow() {
+    props.setShowWindow(true);
+    props.setSelectedOrdersItem(order);
+  }
+
   return (
     <div className="flex flex-row transition duration-500 shadow ease-in-out transform hover:-translate-y-1 hover:shadow-lg select-none cursor-pointer bg-white rounded-md items-center p-4">
       <div className="flex-initial mx-5">
@@ -21,12 +32,19 @@ export default function OrderItem(props: { order: OrdersItem }) {
       </span>
       <div className="purchase-date w-1/3">
         <span data-testid="purchase-date-title">Date of Purchase: </span>
-        <span data-testid="purchase-date">{date.toLocaleDateString()}</span>
+        <span data-testid="purchase-date">{date}</span>
       </div>
       <div className="w-1/5">
         <span data-testid="number-title">Number: </span>
         <span data-testid="purchase-number">{order.purchaseNum}</span>
       </div>
+      <button
+        className="my-order-view-detail whitespace-nowrap ml-24 px-5 py-2.5 mb-2 bg-blue-600 hover:bg-blue-800 text-white transition ease-in duration-200 font-semibold shadow-md rounded-lg"
+        data-testid="view-detail"
+        onClick={myOrderDetailWindow}
+      >
+        View Detail
+      </button>
     </div>
   );
 }
