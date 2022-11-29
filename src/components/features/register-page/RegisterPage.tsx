1e718ba9be156af23d36dc6fd1557a0cef7cd8e1
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-11-29 16:09:19 +0800

    feat: reset input when register successfully and adjust input form style

--- src/components/features/register-page/RegisterPage.tsx
+++ src/components/features/register-page/RegisterPage.tsx
@@ -4,10 +4,10 @@ import React, { useState } from 'react';
 import Banner from '@/components/common/banner/Banner';
 
 const basicClassName =
-  'form-control block px-4 py-2 mb-5 text-base text-gray-900 font-normal border-2' +
+  'form-control block px-4 py-2 mt-5 h-10 text-base text-gray-900 font-normal border-2' +
   ' border-solid border-gray-500 rounded focus:border-purple-400 focus:outline-none';
 const getClassName = (error: string | undefined) =>
-  error ? basicClassName + ' border-red-500' : basicClassName;
+  error ? basicClassName + ' border-red-500 mt-0' : basicClassName;
 
 const initialError = {
   usernameError: '',
@@ -46,11 +46,15 @@ export default function RegisterPage() {
         .then((data) => {
           if (data.data.message === 'error.http.200') {
             // 先判断status，再判断tab
-            if (data.data.title === '用户名已存在') {
+            if (data.data.title === 'username exists') {
               return setError({ usernameError: 'Username already exist!' });
             }
           }
           setRegisterSuccess(true);
+          resetInput();
+          setTimeout(() => {
+            setRegisterSuccess(false);
+          }, 2000);
         })
         .catch(() => {
           setRegisterSuccess(false);
@@ -64,6 +68,13 @@ export default function RegisterPage() {
   const isPasswordMatch = () => {
     return password === confirmPassword;
   };
+
+  function resetInput() {
+    setError({});
+    setUsername('');
+    setPassword('');
+    setConfirmPassword('');
+  }
 
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     const { id, value } = e.target;
@@ -79,6 +90,9 @@ export default function RegisterPage() {
   }
 
   const { usernameError, passwordError, confirmPasswordError } = error;
+  const formClassName = `${
+    usernameError ? 'mt-0' : 'mt-5'
+  } place-content-center ml-24 mr-12 w-[300px]`;
 
   return (
     <div className="Register-page h-screen">
@@ -87,69 +101,72 @@ export default function RegisterPage() {
         success={registerSuccess}
         message={registerSuccess ? successMsg : ''}
       />
-      <div className="Register-page-body flex col-span-2 mx-60">
+      <div className="Register-page-body flex col-span-2 ml-60">
         <div className="Register-page-left">
           <p className="mt-24 text-5xl font-semibold">Welcome</p>
           <div className="flex col-span-2">
             <div className="ml-2 mt-8 w-8 h-24 border-l-8 border-fuchsia-600"></div>
-            <p className="mt-8 py-8 text-l font-semibold">
+            <p className="mt-8 py-8 text-l font-semibold w-[340px]">
               Try this shopping platform for TWers
             </p>
           </div>
           <p className="mt-8 text-5xl text-fuchsia-600">Try it now.</p>
         </div>
 
-        <div className="Register-page-right bg-gray-100 ml-40 mt-10 w-[400px] h-[480px] rounded-xl">
-          <p className="mt-8 text-center text-2xl font-semibold">
+        <div className="Register-page-right bg-gray-100 ml-20 mt-12 w-[400px] h-[480px] rounded-xl">
+          <p className="mt-6 text-center text-2xl font-semibold">
             Register with Us
           </p>
           <p className="mt-6 mx-[50px] text-center text-m text-gray-500">
             Hey TWer, please fill the necessary information to register
           </p>
-          <div className="mt-6 place-content-center ml-24 mr-12 w-[300px]">
-            <form className="mt-4">
-              <>
-                {usernameError && (
-                  <p className="text-red-500">{usernameError}</p>
-                )}
-                <input
-                  type="text"
-                  className={getClassName(usernameError)}
-                  placeholder="Username"
-                  id="username"
-                  value={username}
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </>
-              <>
-                {passwordError && (
-                  <p className="text-red-500">{passwordError}</p>
-                )}
-                <input
-                  type="password"
-                  className={getClassName(passwordError)}
-                  placeholder="Password"
-                  id="password"
-                  value={password}
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </>
-              <>
-                {confirmPasswordError && (
-                  <p className="text-red-500">{confirmPasswordError}</p>
-                )}
-                <input
-                  type="password"
-                  className={getClassName(confirmPasswordError)}
-                  placeholder="Confirm Password"
-                  id="confirmPassword"
-                  value={confirmPassword}
-                  onChange={(e) => handleInputChange(e)}
-                />
-              </>
-            </form>
-          </div>
-          <p className="text-center text-gray-500">
+          <form className={formClassName}>
+            <>
+              {usernameError && (
+                <p className="text-red-500 text-sm h-5">{usernameError}</p>
+              )}
+              <input
+                type="text"
+                className={getClassName(usernameError)}
+                placeholder="Username"
+                id="username"
+                data-testid="username"
+                value={username}
+                onChange={(e) => handleInputChange(e)}
+              />
+            </>
+            <>
+              {passwordError && (
+                <p className="text-red-500 text-sm h-5">{passwordError}</p>
+              )}
+              <input
+                type="password"
+                className={getClassName(passwordError)}
+                placeholder="Password"
+                id="password"
+                data-testid="password"
+                value={password}
+                onChange={(e) => handleInputChange(e)}
+              />
+            </>
+            <>
+              {confirmPasswordError && (
+                <p className="text-red-500 text-sm h-5">
+                  {confirmPasswordError}
+                </p>
+              )}
+              <input
+                type="password"
+                className={getClassName(confirmPasswordError)}
+                placeholder="Confirm Password"
+                id="confirmPassword"
+                data-testid="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => handleInputChange(e)}
+              />
+            </>
+          </form>
+          <p className="text-center text-gray-500 mt-2 mb-8">
             Already has an account?
             <NavLink to="/login" className="text-blue-500 ml-1">
               Login OurShop
@@ -157,7 +174,7 @@ export default function RegisterPage() {
           </p>
           <button
             className="text-center text-white bg-violet-500 hover:bg-violet-700 focus:ring-purple-500
-            transition ease-in font-medium rounded-lg text-l w-52 h-10 mt-12 mx-[100px] py-2 "
+            transition ease-in font-medium rounded-lg text-l w-52 h-10 mx-[100px] py-2 "
             onClick={handleRegister}
           >
             Register
