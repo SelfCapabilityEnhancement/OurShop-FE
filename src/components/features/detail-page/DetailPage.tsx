e0a7b6a4a8f82e10c08d51c59860a4c11990f9ea
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-12-05 15:51:40 +0800

    fix: adjust the layout of detail page

--- src/components/features/detail-page/DetailPage.tsx
+++ src/components/features/detail-page/DetailPage.tsx
@@ -96,13 +96,13 @@ export default function DetailPage() {
         message={validation ? successMsg : failMsg}
       />
       <Loading visible={showLoading} message="Processing..."></Loading>
-      <div className="DetailPage flex mt-12">
+      <div className="DetailPage flex col-span-2 justify-center gap-4">
         <section className="mr-10">
           <Breadcrumb crumbNames={['Product Detail']} />
           <img
             src={product.images.split(',')[bigImgIndex]}
             alt={`big product picture ${bigImgIndex}`}
-            className="h-96 w-[500px] mb-5 rounded-xl border-2 drop-shadow-xl"
+            className="h-96 w-[420px] mb-5 rounded-xl border-2 drop-shadow-xl"
           />
           <div className="flex small-pictures">
             {product.images.split(',').map((imgSrc, index) => (
@@ -122,54 +122,60 @@ export default function DetailPage() {
           </div>
         </section>
         <section className="w-[500px]">
-          <div className="self-center mb-3 font-medium text-3xl">
+          <div className="self-center mb-4 font-medium text-2xl">
             {product.name}
           </div>
-          <p className="price bg-slate-100 rounded-xl h-14 p-3 text-2xl">
+          <p className="price bg-slate-100 rounded-xl h-12 p-2 text-xl">
             Price: ${product.priceMoney} or {product.priceToken} Token
           </p>
-          <div className="self-center my-3 font-medium text-3xl">
+          <div className="self-center my-4 font-medium text-2xl">
             Product Description
           </div>
-          <p className="description bg-slate-100 rounded-xl h-32 p-3 text-2xl">
+          <p className="description bg-slate-100 rounded-xl h-24 p-3 text-xl">
             {product.description}
           </p>
           <div
             data-testid="counter"
-            className="PurchaseNumber flex bottom-12 my-3 items-center"
+            className="PurchaseNumber flex bottom-12 my-4 items-center"
           >
-            <span className="mr-36 font-medium text-3xl">No. of Purchase</span>
+            <span className="mr-36 font-medium text-2xl">No. of Purchase</span>
             <Counter
               count={count}
               handlePlus={handlePlus}
               handleMinus={handleMinus}
             />
           </div>
-          <div className="self-center my-3 font-medium text-3xl">
+          <div className="self-center my-4 font-medium text-2xl">
             Logistic Method
             <span className="text-red-500">*</span>
           </div>
-          <div className="mb-3 ml-2 grid grid-cols-2 gap-x-5">
+          <div className="mb-4 ml-2 grid grid-cols-2 gap-x-5">
             {product.logisticMethod.includes('office') &&
               renderLogisticMethod('office')}
             {product.logisticMethod.includes('address') &&
               renderLogisticMethod('address')}
           </div>
-          <div className="self-center my-3 font-medium text-3xl">Comment</div>
-          <p className="description bg-slate-100 rounded-xl h-20 mb-10 p-3 text-2xl">
+          <div className="self-center my-4 font-medium text-2xl">Comment</div>
+          <p className="description bg-slate-100 rounded-xl h-20 mb-10 p-3 text-xl">
             {product.logisticMethodComment}
           </p>
           <div className="flex justify-between">
             <button
               type="button"
               onClick={handleAddToCart}
-              className="add-in-cart p-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg w-56"
+              className="add-in-cart p-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500
+              focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200
+              text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2
+              focus:ring-offset-2 rounded-lg w-56"
             >
               Add in Shopping Cart
             </button>
             <button
               type="button"
-              className="purchase p-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg w-56"
+              className="purchase p-2 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500
+              focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200
+              text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2
+              focus:ring-offset-2 rounded-lg w-56"
             >
               Purchase
             </button>
