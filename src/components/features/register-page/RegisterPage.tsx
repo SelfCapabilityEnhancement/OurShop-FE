2e12a36fcb488a193d685ffdcbc26370db4f47df
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-12-05 15:09:56 +0800

    fix: modify the compatibility issue of register page

--- src/components/features/register-page/RegisterPage.tsx
+++ src/components/features/register-page/RegisterPage.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import Banner from '@/components/common/banner/Banner';
 
 const basicClassName =
-  'form-control block px-4 py-2 mt-5 h-10 text-base text-gray-900 font-normal border-2' +
+  'form-control block px-4 py-2 mt-5 h-11 w-full text-base text-gray-900 font-normal border-2' +
   ' border-solid border-gray-500 rounded focus:border-purple-400 focus:outline-none bg-gray-100';
 const getClassName = (error: string | undefined) =>
   error ? basicClassName + ' border-red-500 mt-0' : basicClassName;
@@ -98,26 +98,24 @@ export default function RegisterPage() {
   return (
     <div className="Register-page h-screen">
       <Banner visible={registerSuccess} success={true} message={successMsg} />
-      <div className="Register-page-body flex col-span-2 ml-64">
+      <div className="Register-page-body flex col-span-2 justify-center gap-32">
         <div className="Register-page-left">
-          <p className="mt-24 text-5xl font-semibold">Welcome</p>
+          <p className="mt-24 text-6xl font-semibold">Welcome</p>
           <div className="flex col-span-2">
-            <div className="ml-2 mt-8 w-8 h-24 border-l-8 border-fuchsia-600"></div>
-            <p className="mt-8 py-8 text-l font-semibold w-[340px]">
+            <div className="mt-12 w-8 h-28 border-l-8 border-fuchsia-600"></div>
+            <p className="mt-12 py-8 text-xl font-semibold w-[360px]">
               Try this shopping platform for TWers
             </p>
           </div>
-          <p className="mt-8 text-5xl text-fuchsia-600">Try it now.</p>
+          <p className="mt-12 text-6xl text-fuchsia-600">Try it now.</p>
         </div>
 
-        <div className="Register-page-right bg-gray-100 ml-40 mt-12 w-[400px] h-[480px] rounded-xl">
-          <p className="mt-6 text-center text-2xl font-semibold">
-            Register with Us
-          </p>
-          <p className="mt-6 mx-[50px] text-center text-m text-gray-500">
+        <div className="Register-page-right bg-gray-100 text-center mt-16 w-[480px] h-[520px] rounded-xl">
+          <p className="mt-6 text-3xl font-semibold">Register with Us</p>
+          <p className="mt-6 mx-[100px] h-14 text-m text-gray-500">
             Hey TWer, please fill the necessary information to register
           </p>
-          <form className="place-content-center ml-24 mr-12 w-[300px]">
+          <form className="mx-24 text-left">
             <>
               {usernameError && (
                 <p className="text-red-500 text-sm h-5">{usernameError}</p>
@@ -163,15 +161,15 @@ export default function RegisterPage() {
               />
             </>
           </form>
-          <p className="text-center text-gray-500 mt-3 mb-8">
+          <p className="text-gray-500 mt-4 mb-20">
             Already have an account?
             <NavLink to="/login" className="text-blue-500 ml-1">
               Login OurShop
             </NavLink>
           </p>
           <button
-            className="text-center text-white bg-violet-500 hover:bg-violet-700 focus:ring-purple-500
-            transition ease-in font-medium rounded-lg text-l w-52 h-10 mt-9 mx-[100px] py-2 disabled:opacity-50"
+            className="text-white bg-violet-500 hover:bg-violet-700 focus:ring-purple-500
+            transition ease-in font-medium rounded-lg text-l w-56 h-10 py-2 disabled:opacity-50"
             data-testid="register-btn"
             onClick={handleRegister}
             disabled={!buttonEnabled}
