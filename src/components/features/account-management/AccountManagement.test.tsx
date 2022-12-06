db0f0ee8e253ff1017760764017ce6db3eca3343
liuyanlintw <yanlin.liu@thoughtworks.com>
2022-12-06 14:33:22 +0800

    feat: add account management table head test

---
+++ src/components/features/account-management/AccountManagement.test.tsx
@@ -0,0 +1,37 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { BrowserRouter } from 'react-router-dom';
+import AccountManagement from '@/components/features/account-management/AccountManagement';
+
+jest.mock('@/service', () => ({}));
+
+window.IntersectionObserver = jest.fn().mockImplementation(() => ({
+  observe: () => null,
+  disconnect: () => null,
+}));
+
+describe('Account Management', () => {
+  // const user = userEvent.setup();
+
+  beforeEach(async () => {
+    await act(async () => {
+      render(<AccountManagement />, { wrapper: BrowserRouter });
+    });
+  });
+
+  afterEach(cleanup);
+
+  it('should show tabs', () => {
+    expect(screen.getByText('Account List')).toBeInTheDocument();
+    expect(screen.getByText('Role Configuration')).toBeInTheDocument();
+    expect(screen.getByText('Function Configuration')).toBeInTheDocument();
+  });
+
+  it('should show account list tabs', () => {
+    expect(screen.getByText('UserName')).toBeInTheDocument();
+    expect(screen.getByText('Connection')).toBeInTheDocument();
+    expect(screen.getByText('Role')).toBeInTheDocument();
+    expect(screen.getByText('Created at')).toBeInTheDocument();
+    expect(screen.getByText('Action')).toBeInTheDocument();
+  });
+});
--- src/routers/router.tsx
+++ src/routers/router.tsx
@@ -13,6 +13,7 @@ import ProductManagement from '@/components/features/product-management/ProductM
 import LoginPage from '@/components/features/login-page/LoginPage';
 import RegisterPage from '@/components/features/register-page/RegisterPage';
 import App from '@/App';
+import AccountManagement from '@/components/features/account-management/AccountManagement';
 
 export const router = createBrowserRouter([
   {
@@ -71,6 +72,10 @@ export const router = createBrowserRouter([
       {
         path: 'product-management',
         element: <ProductManagement />,
+      },
+      {
+        path: 'account-management',
+        element: <AccountManagement />,
       },
     ],
   },
