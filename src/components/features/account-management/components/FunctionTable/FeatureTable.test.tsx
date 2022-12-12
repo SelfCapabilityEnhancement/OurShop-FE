f410e6f2cec0377a697b6a3575f07ded271d0fff
PeileYang <369270602@qq.com>
2022-12-12 15:46:29 +0800

    test(138):adding tests on table items for displaying functional tables

--- src/components/features/account-management/components/FunctionTable/FeatureTable.test.tsx
+++ src/components/features/account-management/components/FunctionTable/FeatureTable.test.tsx
@@ -2,6 +2,7 @@ import { cleanup, render, screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import { BrowserRouter } from 'react-router-dom';
 import FeatureTable from './FeatureTable';
+import { features } from '@/mocks/mockData';
 
 window.IntersectionObserver = jest.fn().mockImplementation(() => ({
   observe: () => null,
@@ -11,7 +12,7 @@ window.IntersectionObserver = jest.fn().mockImplementation(() => ({
 describe('Function List', () => {
   beforeEach(async () => {
     await act(async () => {
-      render(<FeatureTable />, {
+      render(<FeatureTable featureList={features} />, {
         wrapper: BrowserRouter,
       });
     });
@@ -27,5 +28,15 @@ describe('Function List', () => {
     expect(tableHead[2]).toHaveTextContent('Description');
     expect(tableHead[3]).toHaveTextContent('Updated at');
     expect(tableHead[4]).toHaveTextContent('Action');
+  });
+
+  test('should show function list items', () => {
+    expect(screen.getByText('Product Management')).toBeInTheDocument();
+    expect(screen.getByText('/product/product-management')).toBeInTheDocument();
+    expect(screen.getByText('2022-12-07 06:52:37')).toBeInTheDocument();
+    expect(screen.getByText('Order Management')).toBeInTheDocument();
+    expect(screen.getByText('/order/order-management')).toBeInTheDocument();
+    expect(screen.getByText('2022-12-07 06:53:32')).toBeInTheDocument();
+    expect(screen.getAllByText('Edit')).toHaveLength(2);
   });
 });
