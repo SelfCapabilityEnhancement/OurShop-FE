a68debaebc0a41adbc69e790955da2c80817c40b
PeileYang <369270602@qq.com>
2022-12-14 17:47:30 +0800

    test(138): modify test of role table

--- src/components/features/account-management/components/RoleTable/RoleTable.test.tsx
+++ src/components/features/account-management/components/RoleTable/RoleTable.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import RoleTable from './RoleTable';
-import { roles } from '@/mocks/mockData';
+import { features, roles } from '@/mocks/mockData';
 import { Role } from '@/components/common/CustomTypes';
 import * as service from '@/service';
 import { act } from 'react-dom/test-utils';
@@ -8,11 +8,13 @@ import { BrowserRouter } from 'react-router-dom';
 
 jest.mock('@/service', () => ({
   getRoleList: jest.fn(),
+  getFeatureList: jest.fn(),
 }));
 
 describe('Role List', () => {
   beforeEach(async () => {
     jest.spyOn(service, 'getRoleList').mockResolvedValue(roles);
+    jest.spyOn(service, 'getFeatureList').mockResolvedValue(features);
     await act(async () => {
       render(<RoleTable />, {
         wrapper: BrowserRouter,
