c697b312d4a342a3027df07d93ff7a71894ee722
PeileYang <369270602@qq.com>
2022-12-14 09:39:43 +0800

    test(138): add test of show old feature info

---
+++ src/components/features/account-management/components/FeatureTable/EditFeature.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import EditFeature from '@/components/features/account-management/components/FeatureTable/EditFeature';
+import { features } from '@/mocks/mockData';
+
+window.IntersectionObserver = jest.fn().mockImplementation(() => ({
+  observe: jest.fn(),
+  disconnect: jest.fn(),
+}));
+
+jest.mock('@/service', () => ({
+  updateFeature: jest.fn(),
+}));
+
+describe('Edit Function', () => {
+  beforeEach(() => {
+    render(
+      <EditFeature
+        isOpen={true}
+        handleClose={jest.fn}
+        oldFeature={features[0]}
+      />
+    );
+  });
+  test('should show old feature info', () => {
+    expect(
+      screen.getByPlaceholderText('/product/product-management')
+    ).toBeInTheDocument();
+    expect(screen.getByPlaceholderText('product')).toBeInTheDocument();
+  });
+});
