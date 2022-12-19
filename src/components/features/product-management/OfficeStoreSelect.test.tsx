0bdb2cf0c76fff03d01a8c57c0ee09c7a2ae8d84
PeileYang <369270602@qq.com>
2022-12-19 15:22:49 +0800

    test(141): add test for office select

---
+++ src/components/features/product-management/OfficeStoreSelect.test.tsx
@@ -0,0 +1,45 @@
+import { cleanup, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import { BrowserRouter } from 'react-router-dom';
+import { mockStoreItems } from '@/mocks/mockData';
+import { officeList } from '@/components/features/create-product/CreateProduct';
+import OfficeStoreSelect from '@/components/features/product-management/OfficeStoreSelect';
+
+jest.mock('@/service', () => ({
+  getAllOffices: jest.fn(),
+}));
+
+window.IntersectionObserver = jest.fn().mockImplementation(() => ({
+  observe: () => null,
+  disconnect: () => null,
+}));
+
+describe('Office store item', () => {
+  beforeEach(async () => {
+    await act(async () => {
+      render(
+        <OfficeStoreSelect
+          storeItem={mockStoreItems[0]}
+          officeList={officeList}
+          error={{ office: false, inventory: false }}
+          isMinCounts={true}
+          isMaxCounts={false}
+          setStoreItem={jest.fn}
+          addStoreItem={jest.fn}
+          deleteStoreItem={jest.fn}
+        />,
+        { wrapper: BrowserRouter }
+      );
+    });
+  });
+
+  afterEach(cleanup);
+
+  it('should show Office 1 item', async () => {
+    expect(screen.getByTestId('drop-down')).toBeInTheDocument();
+    expect(screen.getByText('has')).toBeInTheDocument();
+    expect(screen.getByText('Available')).toBeInTheDocument();
+    expect(screen.getByTestId('add-store-item')).toBeInTheDocument();
+    expect(screen.getAllByTestId('drop-down-item')).toHaveLength(6);
+  });
+});
