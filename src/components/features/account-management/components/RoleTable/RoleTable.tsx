605b4ec9cc11038889ab3047239a548289c3a5d0
PeileYang <369270602@qq.com>
2022-12-20 17:14:23 +0800

    feat(141): modify style of role table

--- src/components/features/account-management/components/RoleTable/RoleTable.tsx
+++ src/components/features/account-management/components/RoleTable/RoleTable.tsx
@@ -60,7 +60,9 @@ export default function RoleTable() {
           {roleList.map((role, index) => (
             <tr className="h-16 border-b border-gray-400" key={role.roleId}>
               <td className="px-2">{role.roleName}</td>
-              <td className="px-2">{role.featureNameList.join(',')}</td>
+              <td className="px-2 w-[500px]">
+                {role.featureNameList.join(', ')}
+              </td>
               <td className="px-2">{role.updateTime}</td>
               <td className="px-2 text-blue-600">
                 <button onClick={() => handleEdit(index)}>Edit</button>
