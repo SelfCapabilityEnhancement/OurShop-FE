c155640b5d090140a7f7740f3c2e3f2235e04186
PeileYang <369270602@qq.com>
2022-12-23 10:41:16 +0800

    test(141): add test to pass jest

--- src/components/features/product-management/EditProduct.test.tsx
+++ src/components/features/product-management/EditProduct.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from '@testing-library/react';
 import EditProduct from '@/components/features/product-management/EditProduct';
 import { tempProducts } from '@/mocks/mockData';
 import { Product } from '@/components/common/CustomTypes';
+import userEvent from '@testing-library/user-event';
+import * as service from '@/service';
 
 window.IntersectionObserver = jest.fn().mockImplementation(() => ({
   observe: jest.fn(),
@@ -13,6 +15,8 @@ jest.mock('@/service', () => ({
 }));
 
 describe('Edit Product', () => {
+  const user = userEvent.setup();
+
   beforeEach(() => {
     render(
       <EditProduct
@@ -42,5 +46,29 @@ describe('Edit Product', () => {
         screen.getByDisplayValue(product[v] as string)
       ).toBeInTheDocument();
     });
+  });
+
+  it('should show old product info', async () => {
+    const items: { [key: string]: keyof Product } = {
+      'Product Name': 'name',
+      'Price in USD': 'priceMoney',
+      'Price in Token': 'priceToken',
+      'Product Description': 'description',
+    };
+
+    Object.entries(items).forEach(([k, v]) => {
+      expect(screen.getByText(k)).toBeInTheDocument();
+      const product: Product = tempProducts[0];
+      expect(
+        screen.getByDisplayValue(product[v] as string)
+      ).toBeInTheDocument();
+    });
+
+    const updateProductMock = jest.spyOn(service, 'updateProduct');
+
+    const saveBtn = await screen.findByTestId('save-btn');
+    await user.click(saveBtn);
+
+    expect(updateProductMock).toBeCalled();
   });
 });
