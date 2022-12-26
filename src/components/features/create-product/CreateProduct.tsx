2e880b78216ee9a32c7689e61c43a9c5f998fe2e
wujunlan <2372566752@qq.com>
2022-12-26 17:10:08 +0800

    fix: create product button will be obscured when office has six

--- src/components/features/create-product/CreateProduct.tsx
+++ src/components/features/create-product/CreateProduct.tsx
@@ -386,15 +386,16 @@ function CreateProduct() {
                   />
                 </>
               ))}
-              <footer className="flex float-left absolute mt-[640px]">
-                <button
-                  onClick={(event) => handleCreateProduct(event)}
-                  className=" create text-white bg-violet-500 hover:bg-violet-700 focus:ring-violet-500 transition ease-in duration-200 font-medium rounded-lg text-lg w-64 px-5 py-2.5 text-center"
-                >
-                  Create Product
-                </button>
-              </footer>
             </div>
+
+            <footer className="flex float-left">
+              <button
+                onClick={(event) => handleCreateProduct(event)}
+                className=" create text-white bg-violet-500 hover:bg-violet-700 focus:ring-violet-500 transition ease-in duration-200 font-medium rounded-lg text-lg w-64 px-5 py-2.5 text-center"
+              >
+                Create Product
+              </button>
+            </footer>
           </Tab.Panel>
 
           <Tab.Panel>Approval Flow</Tab.Panel>
