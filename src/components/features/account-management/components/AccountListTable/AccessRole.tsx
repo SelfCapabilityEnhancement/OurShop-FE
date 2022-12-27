82ffe939fd5a26f6513fcf409e671a72aff2516a
Freewhisper77 <emmasddx@foxmail.com>
2022-12-27 14:29:51 +0800

    feat(145): when user click save button, show success Banner and give roleIds to backend

--- src/components/features/account-management/components/AccountListTable/AccessRole.tsx
+++ src/components/features/account-management/components/AccountListTable/AccessRole.tsx
@@ -3,7 +3,8 @@ import { Dialog, Transition } from '@headlessui/react';
 import CancelIcon from '@/components/common/cancel-icon/Cancel-icon';
 import { Account, Role } from '@/components/common/CustomTypes';
 import { initAccount } from '@/constants';
-import { getRoleList } from '@/service';
+import { getRoleList, updateRoleNames } from '@/service';
+import Banner from '@/components/common/banner/Banner';
 
 const basicRoleName =
   'w-40 h-9 text-center text-sm text-white font-normal py-2 rounded-lg mb-80';
@@ -17,8 +18,11 @@ export default function AccessRole({
   handleClose: Function;
   oldAccount: Account;
 }) {
+  const [showBanner, setShowBanner] = useState(false);
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [message, setMessage] = useState<string>();
   const [allRoles, setAllRoles] = useState<Role[]>([]);
-  const [roleIds, setRoleIds] = useState<number[]>([]);
+  const [roleIds, setRoleIds] = useState<number[]>([1]);
   const [account, setAccount] = useState<Account>(initAccount);
 
   useEffect(() => {
@@ -46,10 +50,33 @@ export default function AccessRole({
   };
 
   const handleSelect = (id: number) => {
+    if (id === 1) return;
     if (!roleIds.includes(id)) {
       setRoleIds([...roleIds, id]);
     } else {
       setRoleIds(roleIds.filter((x) => x !== id));
+    }
+  };
+
+  const handleSubmit = async () => {
+    try {
+      await updateRoleNames(account.userId, roleIds);
+      setUpdateSuccess(true);
+      setMessage('The Change was made Successfully!');
+      setShowBanner(true);
+      setTimeout(() => {
+        setShowBanner(false);
+        window.location.reload();
+      }, 3000);
+    } catch (e) {
+      setAccount(oldAccount);
+      setUpdateSuccess(false);
+      setMessage('Wrong!');
+      setShowBanner(false);
+      setTimeout(() => {
+        setShowBanner(false);
+        window.location.reload();
+      }, 3000);
     }
   };
 
@@ -91,20 +118,29 @@ export default function AccessRole({
                 <CancelIcon handleClose={handleClose} />
               </div>
             </Dialog.Title>
-
-            <div className="col-span-2">
-              <div className="text-xl text-gray-900 mb-8">
-                Please select role for the user {account.username}
+            <div className="mt-2">
+              <Banner
+                visible={showBanner}
+                success={updateSuccess}
+                message={message as string}
+              />
+              <div className="col-span-2">
+                <div className="text-xl text-gray-900 mb-8">
+                  Please select role for the user {account.username}
+                </div>
+                <div className="flex justify-start gap-6 mt-3">
+                  {allRoles.map((role) => renderRole(role, role.roleId))}
+                </div>
               </div>
-              <div className="flex justify-start gap-6 mt-3">
-                {allRoles.map((role) => renderRole(role, role.roleId))}
-              </div>
-            </div>
 
-            <div className="flex justify-end mr-8">
-              <button className="text-white bg-violet-500 hover:bg-violet-700 focus:ring-violet-500 transition ease-in duration-200 font-medium rounded-2xl text-2xl w-44 h-12 px-5 py-2 text-center">
-                Save
-              </button>
+              <div className="flex justify-end mr-8">
+                <button
+                  onClick={() => handleSubmit()}
+                  className="text-white bg-violet-500 hover:bg-violet-700 focus:ring-violet-500 transition ease-in duration-200 font-medium rounded-2xl text-2xl w-44 h-12 px-5 py-2 text-center"
+                >
+                  Save
+                </button>
+              </div>
             </div>
           </Dialog.Panel>
         </Transition.Child>
