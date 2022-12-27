92b0cc95e21970765a7d08c9e1ec85f1a8295d79
Freewhisper77 <emmasddx@foxmail.com>
2022-12-27 12:20:27 +0800

    feat(145): make roleNames has different bg-color when this user has part of roleNames

--- src/components/features/account-management/components/AccountListTable/AccessRole.tsx
+++ src/components/features/account-management/components/AccountListTable/AccessRole.tsx
@@ -1,10 +1,12 @@
 import { Fragment, useEffect, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import CancelIcon from '@/components/common/cancel-icon/Cancel-icon';
-import { Account } from '@/components/common/CustomTypes';
+import { Account, Role } from '@/components/common/CustomTypes';
 import { initAccount } from '@/constants';
+import { getRoleList } from '@/service';
 
-const roles = ['Buyer', 'Platform Ops', 'Site Admin'];
+const basicRoleName =
+  'w-40 h-9 text-center text-sm text-white font-normal py-2 rounded-lg mb-80';
 
 export default function AccessRole({
   isOpen,
@@ -15,21 +17,40 @@ export default function AccessRole({
   handleClose: Function;
   oldAccount: Account;
 }) {
+  const [allRoles, setAllRoles] = useState<Role[]>([]);
+  const [roleIds, setRoleIds] = useState<number[]>([]);
   const [account, setAccount] = useState<Account>(initAccount);
 
   useEffect(() => {
+    getRoleList(false).then((data) => {
+      setAllRoles(data);
+    });
     setAccount(oldAccount);
-  }, [isOpen]);
+    setRoleIds(oldAccount.roles.map((role) => role.roleId));
+  }, [oldAccount, isOpen]);
 
-  const renderRole = (item: string) => {
+  const renderRole = (role: Role, id: number) => {
     return (
       <button
-        key={item}
-        className="w-44 h-9 text-center text-sm text-white font-normal py-2 rounded-lg mb-80 bg-[#AE66C3]"
+        key={role.roleId}
+        onClick={() => handleSelect(id)}
+        className={
+          roleIds.includes(id)
+            ? basicRoleName + ' bg-[#AE66C3]'
+            : basicRoleName + ' bg-gray-300'
+        }
       >
-        {item}
+        {role.roleName}
       </button>
     );
+  };
+
+  const handleSelect = (id: number) => {
+    if (!roleIds.includes(id)) {
+      setRoleIds([...roleIds, id]);
+    } else {
+      setRoleIds(roleIds.filter((x) => x !== id));
+    }
   };
 
   return (
@@ -60,7 +81,7 @@ export default function AccessRole({
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          <Dialog.Panel className="fixed top-[19%] w-[600px] transform overflow-hidden rounded-2xl bg-gray-200 p-6 align-middle transition-all">
+          <Dialog.Panel className="fixed top-[19%] w-[590px] transform overflow-hidden rounded-2xl bg-gray-200 p-6 align-middle transition-all">
             <Dialog.Title as="h1" className="leading-6 grid grid-cols-3">
               <div></div>
               <div className="text-[#A45FB7] flex text-[18px] content-center font-semibold my-[20%]">
@@ -76,7 +97,7 @@ export default function AccessRole({
                 Please select role for the user {account.username}
               </div>
               <div className="flex justify-start gap-6 mt-3">
-                {roles.map((item) => renderRole(item))}
+                {allRoles.map((role) => renderRole(role, role.roleId))}
               </div>
             </div>
 
