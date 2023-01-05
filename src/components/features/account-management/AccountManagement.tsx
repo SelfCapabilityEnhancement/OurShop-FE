6c20f068c1d7b4f3f907ee93733d5fac860a92d0
Freewhisper77 <emmasddx@foxmail.com>
2023-01-05 11:53:50 +0800

    fix(145): remove if besides useEffect

--- src/components/features/account-management/AccountManagement.tsx
+++ src/components/features/account-management/AccountManagement.tsx
@@ -21,75 +21,81 @@ export default function AccountManagement() {
   const [roleList, setRoleList] = useState<Role[]>([]);
   const [featureList, setFeatureList] = useState<Feature[]>([]);
   const [showBanner, setShowBanner] = useState(false);
+  const [showClick, setShowClick] = useState(false);
 
   const routerList = localStorage.getItem('router');
-  if (routerList === null) {
-    useEffect(() => {
+
+  useEffect(() => {
+    if (routerList === null) {
       setShowBanner(true);
       setTimeout(() => {
         setShowBanner(false);
         navigate('/login');
       }, 2000);
-    }, []);
-    return (
-      <Banner visible={showBanner} success={false} message={'Not Login'} />
-    );
-  } else if (!routerList.includes('account-management')) {
-    return (
-      <>
-        You are forbidden to access this page.Please click{' '}
-        <NavLink to="/home">here</NavLink> to home page.
-      </>
-    );
-  } else {
-    // @ts-ignore
-    useEffect(() => {
-      getAccountList().then((data) => {
-        setUserList(data);
-      });
-      getRoleList(false).then((data) => {
-        setRoleList(data);
-      });
-      getFeatureList().then((data) => {
-        setFeatureList(data);
-      });
-    }, []);
+    } else if (!routerList.includes('account-management')) {
+      setShowClick(true);
+    }
+  }, []);
 
-    return (
-      <div className="w-full max-w-full p-3">
-        <Tab.Group>
-          <Tab.List className="flex gap-24 p-1 ml-16">
-            {tabs.map((tab) => {
-              return (
-                <Tab
-                  key={tab.id}
-                  className={({ selected }) =>
-                    classNames(
-                      'rounded-lg text-xl font-semibold outline-0 mt-4',
-                      selected
-                        ? `${tab.id} text-pink-500 underline underline-offset-8 border-b-2 border-white`
-                        : 'text-gray-800'
-                    )
-                  }
-                >
-                  {tab.name}
-                </Tab>
-              );
-            })}
-          </Tab.List>
-          <Tab.Panels>
-            <Tab.Panel>
-              <AccountListTable userList={userList} />
-            </Tab.Panel>
-            <Tab.Panel>
-              <RoleTable roleList={roleList} />
-            </Tab.Panel>
-            <Tab.Panel>
-              <FeatureTable featureList={featureList} />
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
-      </div>
-    );
-  }
+  useEffect(() => {
+    getAccountList().then((data) => {
+      setUserList(data);
+    });
+    getRoleList(false).then((data) => {
+      setRoleList(data);
+    });
+    getFeatureList().then((data) => {
+      setFeatureList(data);
+    });
+  }, []);
+
+  return (
+    <>
+      {routerList === null && (
+        <Banner visible={showBanner} success={false} message={'Not Login'} />
+      )}
+      {showClick && (
+        <>
+          You are forbidden to access this page.Please click{' '}
+          <NavLink to="/home">here</NavLink> to home page.
+        </>
+      )}
+      {!showClick && (
+        <div className="w-full max-w-full p-3">
+          <Tab.Group>
+            <Tab.List className="flex gap-24 p-1 ml-16">
+              {tabs.map((tab) => {
+                return (
+                  <Tab
+                    key={tab.id}
+                    className={({ selected }) =>
+                      classNames(
+                        'rounded-lg text-xl font-semibold outline-0 mt-4',
+                        selected
+                          ? `${tab.id} text-pink-500 underline underline-offset-8 border-b-2 border-white`
+                          : 'text-gray-800'
+                      )
+                    }
+                  >
+                    {tab.name}
+                  </Tab>
+                );
+              })}
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>
+                <AccountListTable userList={userList} />
+              </Tab.Panel>
+              <Tab.Panel>
+                <RoleTable roleList={roleList} />
+              </Tab.Panel>
+              <Tab.Panel>
+                <FeatureTable featureList={featureList} />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
+      )}
+    </>
+  );
 }
