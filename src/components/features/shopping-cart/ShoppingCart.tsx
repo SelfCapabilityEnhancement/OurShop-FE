75d16ee8950d7343571ea5a4761b50aa5e321b72
PeileYang <369270602@qq.com>
2023-01-06 15:52:18 +0800

    refactor(142): modify split with `,` and blank space

--- src/components/features/shopping-cart/ShoppingCart.tsx
+++ src/components/features/shopping-cart/ShoppingCart.tsx
@@ -103,7 +103,7 @@ export default function ShoppingCart() {
         (_item, index) => updatedCheckedState[index]
       );
       const selectedOffices = selectedItems.map(
-        (e) => new Set(e.offices?.split(','))
+        (e) => new Set(e.offices?.split(', '))
       );
       let collectOffices = selectedOffices[0];
 
